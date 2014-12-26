05d840847157210a4cae607318d351d8b53eee26
Chirag Aggarwal <chi6rag@gmail.com>
2014-12-26 08:28:53 +0530

    Enforce password restrictions

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -61,7 +61,7 @@ class UserTest < ActiveSupport::TestCase
     assert_not duplicate_user.valid?
   end
 
-  test "password should have minimum length" do
+  test "should have minimum length" do
     @user.password = @user.password_confirmation = "a"*5
     assert_not @user.valid?
   end
