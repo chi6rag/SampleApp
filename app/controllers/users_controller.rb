6658e2e2898b4d93450b929098b0aae499ed50fa
Chirag Aggarwal <chi6rag@gmail.com>
2014-12-30 22:48:09 +0530

    Implement friendly forwarding

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -43,8 +43,10 @@ class UsersController < ApplicationController
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 
+  # confirms a logged in user
   def logged_in_user
     unless logged_in?
+      store_location
       flash[:danger] = "Please log in"
       redirect_to login_url
     end
@@ -57,5 +59,4 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     redirect_to root_url unless current_user?(@user)
   end
-
 end
