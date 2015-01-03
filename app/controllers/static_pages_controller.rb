8c364088e3d4bd18115966835e8eef095692ee0b
Chirag Aggarwal <chi6rag@gmail.com>
2015-01-03 12:53:40 +0530

    Implement web based posting of microposts

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,7 @@
 class StaticPagesController < ApplicationController
   def home
+    # create a micropost instance variable only if logged in
+    @micropost = current_user.microposts.build if logged_in?
   end
 
   def help
