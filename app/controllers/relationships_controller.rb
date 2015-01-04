f8fa7ca8f2522bbf57f8436539b0094bad0d18ff
Chirag Aggarwal <chi6rag@gmail.com>
2015-01-04 20:49:52 +0530

    Add AJAX for follow/unfollow buttons

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -4,15 +4,21 @@ class RelationshipsController < ApplicationController
   # find the user to follow by params followed_id
   # follow the user
   def create
-    user = User.find(params[:followed_id])
-    current_user.follow(user)
-    redirect_to user
+    @user = User.find(params[:followed_id])   # made variable for ajax
+    current_user.follow(@user)
+    respond_to do |format|
+      format.html { redirect_to @user }
+      format.js
+    end
   end
 
   def destroy
-    user = Relationship.find(params[:id]).followed
-    current_user.unfollow(user)
-    redirect_to user
+    @user = Relationship.find(params[:id]).followed
+    current_user.unfollow(@user)
+    respond_to do |format|
+      format.html { redirect_to @user }
+      format.js
+    end
   end
 
 end
