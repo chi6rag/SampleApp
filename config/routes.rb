1c75759757bb93c9901b2e13197dc5ccb57edd73
Chirag Aggarwal <chi6rag@gmail.com>
2015-01-04 17:46:54 +0530

    Correct missing passive_relationships association in User, Add stats of followers and following

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,11 @@ Rails.application.routes.draw do
   get 'about' => 'static_pages#about'
   get 'contact' => 'static_pages#contact'
   get 'signup' => 'users#new'
-  resources :users
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
   get 'login' => 'sessions#new'
   post 'login' => 'sessions#create'
   delete 'logout' => 'sessions#destroy'
